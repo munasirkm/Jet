ecaacdf21153b1a321adbbd37c59cfab1f207c31
munasirkm <munasirkm@gmail.com>
2026-04-25 02:51:01 +0530

    chore: Vite proxy config resolve CORS error

--- findFood/vite.config.js
+++ findFood/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
