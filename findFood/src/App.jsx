ad83d592286c00cc998a8823e88fb57fbe0de577
munasirkm <munasirkm@gmail.com>
2026-04-26 04:08:30 +0530

    feat: loading spinner

--- findFood/src/App.jsx
+++ findFood/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import RestaurantList from './components/RestaurantList/RestaurantList';
 import SearchBar from './components/SearchBar/SearchBar';
 import EmptyList from './components/ErrorPage/EmptyList';
+import Loading from './components/Loading/Loading';
 import { fetchRestaurants } from './services/restaurantService';
 import './App.css';
 import Header from './components/Header/Header';
@@ -9,26 +10,32 @@ import Header from './components/Header/Header';
 function App() {
   const [restaurants, setRestaurants] = useState([]);
   const [hasSearched, setHasSearched] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (postcode) => {
     setHasSearched(true);
+    setLoading(true);
 
     try {
       const data = await fetchRestaurants(postcode);
       setRestaurants(data);
     } catch (error) {
       console.error('Search failed:', error);
+    } finally {
+      setLoading(false);
     }
   };
+
 
   return (
     <div className="App">
       <Header />
       <SearchBar onSearch={handleSearch} />
-      <RestaurantList restaurants={restaurants} />
-      {hasSearched && restaurants.length === 0 && <EmptyList />}
+      {loading ? <Loading /> : <RestaurantList restaurants={restaurants} />}
+      {hasSearched && !loading && restaurants.length === 0 && <EmptyList />}
     </div>
   );
 }
+
 
 export default App;
---
+++ findFood/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.css';
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <span className="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
