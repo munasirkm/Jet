97367ef207e8e988100c4c19552aa9dabbcf4719
munasirkm <munasirkm@gmail.com>
2026-04-26 18:59:17 +0530

    fix: remove hardcorded no of results

--- findFood/src/services/restaurantService.js
+++ findFood/src/services/restaurantService.js
@@ -1,10 +1,12 @@
 import { transformRestaurantData } from '../utils/dataTransforms.js';
 
+const NO_OF_RESULTS = 10;
+
 export async function fetchRestaurants(postcode) {
     try {
         const res = await fetch(`/api/${postcode}`);
         const data = await res.json();
-        return transformRestaurantData(data.restaurants.slice(0, 10));
+        return transformRestaurantData(data.restaurants.slice(0, NO_OF_RESULTS));
     } catch (error) {
         console.error('Error fetching restaurants:', error);
         throw error;
